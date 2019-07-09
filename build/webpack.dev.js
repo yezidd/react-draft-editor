699ca5cb6a6c514610253123f4fb4d70eaf39451
myj <myjyixi@163.com>
2019-07-10 01:22:20 +0800

    add webpack

---
+++ build/webpack.dev.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        use: [
+          "babel-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(jpeg|jpg|png|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10240
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|otf)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10240
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "../index.html"),
+      filename: "index.html",
+      chunks: "all",
+      inject: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: true, // 移除空格
+        minifyCSS: true,
+        minifyJS: true,
+        preserveLineBreaks: false, // 保留一个换行符
+        removeComments: true,
+        removeRedundantAttributes: true // 移除多余的属性
+      }
+    })
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
+  devtool: "source-map"
+};
